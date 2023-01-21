92aeeb4b8f5c3fe6d8f71033ed08123a8ab218e2
Marcio Fão <marcio.lopes.fao@gmail.com>
2023-01-21 20:33:53 -0300

    add required plugin info

--- user-wp-block.php
+++ user-wp-block.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 /*
  * Plugin Name:       User WP Block
  * Description:       Gutenberg block for displaying user information in Gutenberg editor
- * Version:           0.1
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * License:           MIT License
+ * License URI:       https://github.com/marciofao/user-wp-block/blob/main/LICENSE
  * Author:            Márcio Lopes Fão
  * Author URI:        https://www.linkedin.com/in/m%C3%A1rcio-lopes-f%C3%A3o-bb559133
  * Text Domain:       uwb
