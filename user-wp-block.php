210b997bddb2257fc3ab18ba8ec180ed38c4bff3
Marcio Fão <marcio.lopes.fao@gmail.com>
2023-01-21 18:54:47 -0300

    plugin init

---
+++ user-wp-block.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Plugin Name:       User WP Block
+ * Description:       Gutenberg block for displaying user information in Gutenberg editor
+ * Version:           0.1
+ * Author:            Márcio Lopes Fão
+ * Author URI:        https://www.linkedin.com/in/m%C3%A1rcio-lopes-f%C3%A3o-bb559133
+ * Text Domain:       uwb
+*/
+
